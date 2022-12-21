ef9888abbc9a09fa639456d3dbdfe92b75176979
Ramez Gerges <es-Ramez.Adly2023@alexu.edu.eg>
2022-12-21 15:39:59 +0200

    zebalaaaaaaaaaaaaaaaaa

--- src/projection.py
+++ src/projection.py
@@ -1,12 +1,16 @@
+import time
+
 import cv2
 import numpy as np
 from pathlib import Path
 import shutil
+import copy
 
 import scipy
 from matplotlib import pyplot as plt
 from skimage.morphology import skeletonize
 from sklearn.preprocessing import normalize
+from skimage.feature import match_template, peak_local_max
 
 
 def cleanup():
@@ -109,8 +113,9 @@ def load_letters(path="./Images"):
     letters_dir = Path(path)
     letters = filter(lambda letter: letter.name.endswith(".png"), letters_dir.iterdir())
     return [(letter.absolute(),
-             cv2.threshold(cv2.cvtColor(load_image(str(letter.absolute())), cv2.COLOR_BGR2GRAY), 100, 255,
-                           cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]) for letter in letters]
+             split_into_words(
+                 cv2.threshold(cv2.cvtColor(load_image(str(letter.absolute())), cv2.COLOR_BGR2GRAY), 100, 255,
+                               cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1])[0]) for letter in letters]
 
 
 def get_letter(letters, letter_name):
@@ -123,6 +128,34 @@ def detect_template(img, template, hard_thresh=0.7):
     return list(zip(*loc[::-1]))
 
 
+def bestAlgo(img, letters, hard_thresh=0.70, granularity=0.001):
+    cpy = copy.deepcopy(img)
+    out = []
+    thresh = 0.998
+    while True:
+        if thresh < hard_thresh:
+            break
+
+        (x, y, w, h, l) = None, None, None, None, None
+        best_score = -1
+        for letter, template in letters:
+            res = cv2.matchTemplate(cpy, template, cv2.TM_CCOEFF_NORMED)
+            if res.max() >= best_score and res.max() >= thresh:
+                loc = np.where(res >= res.max())
+                pt = next(zip(*loc[::-1]))
+                best_score = res.max()
+                w_, h_ = template.shape[::-1]
+                (x, y, w, h, l) = (pt[0], pt[1], w_, h_, letter)
+
+        if best_score == -1:
+            thresh -= granularity
+        else:
+            out.append((x, y, w, h, l))
+            cv2.rectangle(cpy, (x, y), (x + w, y + h), (0, 0, 0), cv2.FILLED)
+
+    return out
+
+
 cleanup()
 letters = load_letters()
 
@@ -130,26 +163,161 @@ img = cv2.imread("tifa.png", cv2.IMREAD_COLOR)
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 (_, binary_img) = cv2.threshold(gray_img, 100, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 binary_img = cv2.bitwise_not(binary_img)
-
 resized_image = resize_to_template(binary_img)
 
-boxes = []
-# for letter, template in [get_letter(letters, letter_name="ـفـ")]:
-for letter, template in letters:
-    w, h = template.shape[::-1]
-    boxes = boxes + [(box[0], box[1], w, h, letter) for box in
-                     detect_template(resized_image, template, hard_thresh=0.85)]
-
-for (x, y, w, h, letter) in boxes:
-    cv2.rectangle(resized_image, (x, y), (x + w, y + h), (255, 255, 255), 1)
+########################################################################## 0 ##########################################
+out = bestAlgo(resized_image, letters)
+for i, (x, y, w, h, letter) in enumerate(out):
+    cv2.imwrite(f"./output/letter_{i}.png", resized_image[y:y + h, x:x + w])
+for (x, y, w, h, letter) in out:
+    cv2.rectangle(resized_image, (x, y), (x + w, y + h), (255, 255, 255))
 cv2.imwrite('./output/res.png', resized_image)
 
-# import numpy as np
-# import matplotlib.pyplot as plt
+########################################################################## 1 ##########################################
+# letter_counter = 0
+# boxes = []
+# for letter, template in letters:
+#     w, h = template.shape[::-1]
+#     boxes = boxes + [(box[0], box[1], w, h, letter) for box in
+#                      detect_template(resized_image, template, hard_thresh=0.7)]
 #
-# from skimage.feature import match_template, peak_local_max
+# for (x, y, w, h, letter) in boxes:
+#     cv2.rectangle(resized_image, (x, y), (x + w, y + h), (255, 255, 255))
+# cv2.imwrite('./output/res.png', resized_image)
+
+########################################################################## 2 ##########################################
+# letter_counter = 0
+# boxes = []
 #
-# coin = template
+# # for letter, template in [get_letter(letters, letter_name="ـفـ")]:
+# for letter, template in letters:
+#     w, h = template.shape[::-1]
+#     boxes = boxes + [(box[0], box[1], w, h, letter) for box in
+#                      detect_template(resized_image, template, hard_thresh=0.91)]
+#
+# for (x, y, w, h, letter) in boxes:
+#     cv2.imwrite(f"./output/letter_{letter_counter}.png", resized_image[y:y + h, x:x + w])
+#     letter_counter += 1
+#     cv2.rectangle(resized_image, (x, y), (x + w, y + h), (0, 0, 0), cv2.FILLED)
+# cv2.imwrite('./output/res0.png', resized_image)
+#
+# for letter, template in letters:
+#     w, h = template.shape[::-1]
+#     boxes = boxes + [(box[0], box[1], w, h, letter) for box in
+#                      detect_template(resized_image, template, hard_thresh=0.89)]
+#
+# for (x, y, w, h, letter) in boxes:
+#     cv2.imwrite(f"./output/letter_{letter_counter}.png", resized_image[y:y + h, x:x + h])
+#     letter_counter += 1
+#     cv2.rectangle(resized_image, (x, y), (x + w, y + h), (0, 0, 0), cv2.FILLED)
+# cv2.imwrite('./output/res1.png', resized_image)
+#
+# for letter, template in letters:
+#     w, h = template.shape[::-1]
+#     boxes = boxes + [(box[0], box[1], w, h, letter) for box in
+#                      detect_template(resized_image, template, hard_thresh=0.88)]
+#
+# for (x, y, w, h, letter) in boxes:
+#     cv2.imwrite(f"./output/letter_{letter_counter}.png", resized_image[y:y + h, x:x + h])
+#     letter_counter += 1
+#     cv2.rectangle(resized_image, (x, y), (x + w, y + h), (0, 0, 0), cv2.FILLED)
+# cv2.imwrite('./output/res2.png', resized_image)
+#
+# for letter, template in letters:
+#     w, h = template.shape[::-1]
+#     boxes = boxes + [(box[0], box[1], w, h, letter) for box in
+#                      detect_template(resized_image, template, hard_thresh=0.87)]
+#
+# for (x, y, w, h, letter) in boxes:
+#     cv2.imwrite(f"./output/letter_{letter_counter}.png", resized_image[y:y + h, x:x + h])
+#     letter_counter += 1
+#     cv2.rectangle(resized_image, (x, y), (x + w, y + h), (0, 0, 0), cv2.FILLED)
+# cv2.imwrite('./output/res3.png', resized_image)
+#
+# for letter, template in letters:
+#     w, h = template.shape[::-1]
+#     boxes = boxes + [(box[0], box[1], w, h, letter) for box in
+#                      detect_template(resized_image, template, hard_thresh=0.86)]
+#
+# for (x, y, w, h, letter) in boxes:
+#     cv2.imwrite(f"./output/letter_{letter_counter}.png", resized_image[y:y + h, x:x + h])
+#     letter_counter += 1
+#     cv2.rectangle(resized_image, (x, y), (x + w, y + h), (0, 0, 0), cv2.FILLED)
+# cv2.imwrite('./output/res4.png', resized_image)
+#
+# for letter, template in letters:
+#     w, h = template.shape[::-1]
+#     boxes = boxes + [(box[0], box[1], w, h, letter) for box in
+#                      detect_template(resized_image, template, hard_thresh=0.85)]
+#
+# for (x, y, w, h, letter) in boxes:
+#     cv2.imwrite(f"./output/letter_{letter_counter}.png", resized_image[y:y + h, x:x + h])
+#     letter_counter += 1
+#     cv2.rectangle(resized_image, (x, y), (x + w, y + h), (0, 0, 0), cv2.FILLED)
+# cv2.imwrite('./output/res5.png', resized_image)
+#
+# for letter, template in letters:
+#     w, h = template.shape[::-1]
+#     boxes = boxes + [(box[0], box[1], w, h, letter) for box in
+#                      detect_template(resized_image, template, hard_thresh=0.83)]
+#
+# for (x, y, w, h, letter) in boxes:
+#     cv2.imwrite(f"./output/letter_{letter_counter}.png", resized_image[y:y + h, x:x + h])
+#     letter_counter += 1
+#     cv2.rectangle(resized_image, (x, y), (x + w, y + h), (0, 0, 0), cv2.FILLED)
+# cv2.imwrite('./output/res6.png', resized_image)
+#
+# for letter, template in letters:
+#     w, h = template.shape[::-1]
+#     boxes = boxes + [(box[0], box[1], w, h, letter) for box in
+#                      detect_template(resized_image, template, hard_thresh=0.76)]
+#
+# for (x, y, w, h, letter) in boxes:
+#     cv2.imwrite(f"./output/letter_{letter_counter}.png", resized_image[y:y + h, x:x + h])
+#     letter_counter += 1
+#     cv2.rectangle(resized_image, (x, y), (x + w, y + h), (0, 0, 0), cv2.FILLED)
+# cv2.imwrite('./output/res7.png', resized_image)
+#
+# for letter, template in letters:
+#     w, h = template.shape[::-1]
+#     boxes = boxes + [(box[0], box[1], w, h, letter) for box in
+#                      detect_template(resized_image, template, hard_thresh=0.75)]
+#
+# for (x, y, w, h, letter) in boxes:
+#     cv2.imwrite(f"./output/letter_{letter_counter}.png", resized_image[y:y + h, x:x + h])
+#     letter_counter += 1
+#     cv2.rectangle(resized_image, (x, y), (x + w, y + h), (0, 0, 0), cv2.FILLED)
+# cv2.imwrite('./output/res8.png', resized_image)
+
+########################################################################## 3 ##########################################
+# t = time.time()
+# start = binary_img.shape[1]  # The whole width. This is our starting point because Arabic is RTL.
+# counter = 0
+# THRESH = 1e-4
+# MIN_DISTANCE = 6
+# for _ in range(15):
+#     best_end = start
+#     best_distance = 10000000
+#     best_cut = None
+#     best_letter = ""
+#     for i in range(start - 1, -1, -1):
+#         cut = binary_img[:, i:start]
+#         for letter, img in letters:
+#             distance = cv2.matchShapes(cut, img, cv2.CONTOURS_MATCH_I2, 0)
+#             if distance < best_distance and MIN_DISTANCE < start - i:
+#                 best_distance = distance
+#                 best_cut = cut
+#                 best_end = i
+#                 best_letter = letter.name
+#
+#     cv2.imwrite(f"./output/{counter}.png", best_cut)
+#     start = best_end
+#     counter = counter + 1
+# print(time.time() - t)
+
+
+########################################################################## 4 ##########################################
+# coin = get_letter(letters, "مـ")[1]
 # result = match_template(resized_image, coin)
 # peaks = peak_local_max(result, min_distance=10, threshold_rel=0.5)
 # ij = np.unravel_index(np.argmax(result), result.shape)
@@ -180,47 +348,21 @@ cv2.imwrite('./output/res.png', resized_image)
 # ax3.autoscale(False)
 # ax3.plot(x, y, 'o', markeredgecolor='r', markerfacecolor='none', markersize=10)
 #
-# plt.show()
+# plt.savefig('./output/match_shapes.png')
 
-# binary_img[baseline_idx: baseline_idx + 10, :] = 0
-# cv2.imwrite("lol.png", binary_img)
-
+########################################################################## 5 ##########################################
 # baseline_idx = baseline(binary_img)
-
+# binary_img[baseline_idx: baseline_idx + 10, :] = 0
+# cv2.imwrite("./output/img_with_baseline.png", binary_img)
 # MAX_PLOT_HEIGHT = 100
 # VP = np.sum(binary_img, 0).astype('int32')  # vertical projection
 # heights = VP / np.linalg.norm(VP) * MAX_PLOT_HEIGHT
 # plt.plot(heights)
-# plt.show()
+# plt.savefig('./output/vertical_projection.png')
 
-# cv2.imshow("lol", binary_img[:][350:])
-# cut = binary_img[:, 266:]
-# cut = binary_img[:, 312:328]
-# cut = binary_img[:, 350:384]
-# cut = binary_img[:, 328:350]
-# cut = binary_img[:, 290:312]
-# cv2.imwrite("cut.png", cut)
+########################################################################## 6 ##########################################
+# cut = binary_img[:, 1600:]
+# cv2.imwrite("./output/cut.png", cut)
 # vals = sorted([(letter, img, cv2.matchShapes(cut, img, cv2.CONTOURS_MATCH_I1, 0)) for letter, img in letters], key=lambda x: x[2], reverse=False)
-
-# start = binary_img.shape[1]  # The whole width. This is our starting point because Arabic is RTL.
-# counter = 0
-# THRESH = 1e-4
-# MIN_DISTANCE = 6
-# while True:
-#     best_end = start
-#     best_distance = 10000000
-#     best_cut = None
-#     best_letter = ""
-#     for i in range(start - 1, -1, -1):
-#         cut = binary_img[:, i:start]
-#         for letter, img in letters:
-#             distance = cv2.matchShapes(cut, img, cv2.CONTOURS_MATCH_I2, 0)
-#             if distance < best_distance and MIN_DISTANCE < start - i:
-#                 best_distance = distance
-#                 best_cut = cut
-#                 best_end = i
-#                 best_letter = letter.name
-#
-#     cv2.imwrite(f"./output/{counter}.png", best_cut)
-#     start = best_end
-#     counter = counter + 1
+# for (letter, img, score) in vals:
+#     print(letter.name, score)
