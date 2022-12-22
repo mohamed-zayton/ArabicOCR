ca99b0835e3ba9594007cbe8c0293f26a36e9f85
Ramez Gerges <es-Ramez.Adly2023@alexu.edu.eg>
2022-12-22 02:02:20 +0200

    ze ba laaaaaaaaa

--- src/projection.py
+++ src/projection.py
@@ -1,3 +1,4 @@
+import difflib
 import time
 
 import cv2
@@ -11,6 +12,8 @@ from matplotlib import pyplot as plt
 from skimage.morphology import skeletonize
 from sklearn.preprocessing import normalize
 from skimage.feature import match_template, peak_local_max
+from Levenshtein import distance as levenshtein_distance
+from tqdm.auto import tqdm
 
 
 def cleanup():
@@ -56,11 +59,18 @@ def load_image(path):
 
 
 def resize_to_template(img):
-    HP = np.sum(img, 1).astype('int32')
+    HP = np.sum(remove_dots(img, img.shape[1] / 2), 1).astype('int32')
     HP_no_border = np.where(HP != 0)[0]
     height = HP_no_border[-1] - HP_no_border[0]
-    # return scipy.misc.imresize(img, 25 / height)
-    return cv2.resize(img, None, fx=34 / height, fy=34 / height, interpolation=cv2.INTER_AREA)
+    return cv2.resize(img, None, fx=30 / height, fy=30 / height, interpolation=cv2.INTER_AREA)
+
+
+def get_binarized_image(name):
+    img = cv2.imread(name, cv2.IMREAD_COLOR)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    (_, binary_img) = cv2.threshold(gray_img, 100, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    binary_img = cv2.bitwise_not(binary_img)
+    return binary_img
 
 
 def split_into_lines(img, empty_rows_above_line=1, empty_rows_below_line=1):
@@ -86,7 +96,7 @@ def split_into_lines(img, empty_rows_above_line=1, empty_rows_below_line=1):
     return lines
 
 
-def split_into_words(img, empty_columns_before_word=1, empty_columns_after_word=1):
+def split_into_words(img, empty_columns_before_word=2, empty_columns_after_word=2):
     projection_bins = np.sum(img, 0).astype('int32')  # vertical projection
 
     consecutive_empty_columns = 0
@@ -113,9 +123,8 @@ def load_letters(path="./Images"):
     letters_dir = Path(path)
     letters = filter(lambda letter: letter.name.endswith(".png"), letters_dir.iterdir())
     return [(letter.absolute(),
-             split_into_words(
-                 cv2.threshold(cv2.cvtColor(load_image(str(letter.absolute())), cv2.COLOR_BGR2GRAY), 100, 255,
-                               cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1])[0]) for letter in letters]
+             split_into_words(cv2.cvtColor(load_image(str(letter.absolute())), cv2.COLOR_BGR2GRAY))[0]) for letter in
+            letters]
 
 
 def get_letter(letters, letter_name):
@@ -126,6 +135,33 @@ def detect_template(img, template, hard_thresh=0.7):
     res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
     loc = np.where(res >= max(hard_thresh, res.max() * 0.95))
     return list(zip(*loc[::-1]))
+
+
+def diff_strings(a, b):
+    output = []
+    matcher = difflib.SequenceMatcher(None, a, b)
+    green = '\x1b[38;5;16;48;5;2m'
+    red = '\x1b[38;5;16;48;5;1m'
+    endgreen = '\x1b[0m'
+    endred = '\x1b[0m'
+
+    width = 0
+    for opcode, a0, a1, b0, b1 in matcher.get_opcodes():
+        if opcode == 'equal':
+            output.append(a[a0:a1])
+            width += a1 - a0
+        elif opcode == 'insert':
+            output.append(f'{green}{b[b0:b1]}{endgreen}')
+            width += b1 - b0
+        elif opcode == 'delete':
+            output.append(f'{red}{a[a0:a1]}{endred}')
+            width += a1 - a0
+        elif opcode == 'replace':
+            output.append(f'{green}{b[b0:b1]}{endgreen}')
+            output.append(f'{red}{a[a0:a1]}{endred}')
+            width += a1 - a0
+            width += b1 - b0
+    return ''.join(output), width
 
 
 def bestAlgo(img, letters, hard_thresh=0.70, granularity=0.001):
@@ -166,12 +202,44 @@ binary_img = cv2.bitwise_not(binary_img)
 resized_image = resize_to_template(binary_img)
 
 ########################################################################## 0 ##########################################
-out = bestAlgo(resized_image, letters)
-for i, (x, y, w, h, letter) in enumerate(out):
-    cv2.imwrite(f"./output/letter_{i}.png", resized_image[y:y + h, x:x + w])
-for (x, y, w, h, letter) in out:
-    cv2.rectangle(resized_image, (x, y), (x + w, y + h), (255, 255, 255))
-cv2.imwrite('./output/res.png', resized_image)
+# out = bestAlgo(resized_image, letters)
+# for i, (x, y, w, h, letter) in enumerate(out):
+#     cv2.imwrite(f"./output/letter_{i}.png", resized_image[y:y + h, x:x + w])
+#
+# print("".join([letter.name.replace("ـ", "").replace(".png", "") for (x, y, w, h, letter) in sorted(out, key=lambda box: box[0], reverse=True)]))
+# for (x, y, w, h, letter) in out:
+#     cv2.rectangle(resized_image, (x, y), (x + w, y + h), (255, 255, 255))
+# cv2.imwrite('./output/res.png', resized_image)
+
+########################################################################## 0.1 ##########################################
+lines = [(resize_to_template(split_into_lines(get_binarized_image("Capture.png"))[0]), "أبجد هوز حطي كلمن سعفص قرشت ثخذ ضظغ")]
+distances = []
+for k, (line, ground_truth) in tqdm(enumerate(lines), desc="lines", leave=True):
+    line = np.pad(line, 20)
+    words = split_into_words(line)
+    word_predictions = []
+    for i, word in enumerate(tqdm(words, desc="words", leave=False)):
+        word = np.pad(word, (20, 20))
+        out = bestAlgo(word, letters)
+        for j, (x, y, w, h, letter) in enumerate(sorted(out, key=lambda box: box[0], reverse=True)):
+            cv2.imwrite(f"./output/line_{k}_word_{i}_letter_{j}.png", word[y:y + h, x:x + w])
+
+        word_predictions.append("".join([letter.name.replace("ـ", "").replace(".png", "") for (x, y, w, h, letter) in
+                                         sorted(out, key=lambda box: box[0], reverse=True)]))
+        cpy = copy.deepcopy(word)
+        for (x, y, w, h, letter) in out:
+            cv2.rectangle(cpy, (x, y), (x + w, y + h), (255, 255, 255))
+        cv2.imwrite(f"./output/line_{k}_word_{i}.png", cpy)
+
+    prediction = " ".join(word_predictions)
+    distance = levenshtein_distance(prediction, ground_truth)
+    # diff = diff_strings(prediction, ground_truth)
+    # print("Ground truth: ", ground_truth.ljust(diff[1] - 2))
+    # print("Prediction: ", diff[0])
+    # print("Distance: ", distance)
+    distances.append((distance, len(ground_truth)))
+
+print("Average distance per letter", sum([x / y for x, y in distances]) / len(distances))
 
 ########################################################################## 1 ##########################################
 # letter_counter = 0
@@ -277,17 +345,20 @@ cv2.imwrite('./output/res.png', resized_image)
 #     letter_counter += 1
 #     cv2.rectangle(resized_image, (x, y), (x + w, y + h), (0, 0, 0), cv2.FILLED)
 # cv2.imwrite('./output/res7.png', resized_image)
-#
+
+
+########################################################################## 2.1 ##########################################
+# boxes = []
+# # for letter, template in [get_letter(letters, letter_name="ـلـ")]:
 # for letter, template in letters:
 #     w, h = template.shape[::-1]
 #     boxes = boxes + [(box[0], box[1], w, h, letter) for box in
-#                      detect_template(resized_image, template, hard_thresh=0.75)]
+#                      detect_template(resized_image, template, hard_thresh=0.90)]
 #
-# for (x, y, w, h, letter) in boxes:
-#     cv2.imwrite(f"./output/letter_{letter_counter}.png", resized_image[y:y + h, x:x + h])
-#     letter_counter += 1
-#     cv2.rectangle(resized_image, (x, y), (x + w, y + h), (0, 0, 0), cv2.FILLED)
-# cv2.imwrite('./output/res8.png', resized_image)
+# for i, (x, y, w, h, letter) in enumerate(boxes):
+#     cv2.imwrite(f"./output/letter_{i}.png", resized_image[y:y + h, x:x + h])
+#     cv2.rectangle(resized_image, (x, y), (x + w, y + h), (255, 255, 255))
+# cv2.imwrite('./output/res.png', resized_image)
 
 ########################################################################## 3 ##########################################
 # t = time.time()
